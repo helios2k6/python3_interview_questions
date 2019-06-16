7eb9c10bf2cac59ab293beb5a507daea37c7e7c6
Andrew Johnson <abjohnson@fb.com>
2019-06-16 15:30:59 -0700

    Improving rotate matrix

--- rotateMatrix.py
+++ rotateMatrix.py
@@ -1,63 +1,70 @@
-def printMat(mat):
-    n = len(mat)
-    for row in range(n):
-        acc = ""
-        for col in range(n):
-            acc = f"{acc} {mat[row][col]}"
-        print(acc)
+class SquareMatrix:
+    def __init__(self, length: int) -> None:
+        self.length = length
+        self.coordToValueMap = {}
 
-def getNextCord(coord, n):
-    return (n - 1 - coord[1], coord[0])
+    def setValue(self, row, col, value) -> None:
+        if row not in self.coordToValueMap:
+            self.coordToValueMap[row] = {}
+        self.coordToValueMap[row][col] = value
 
-def getQueue(n):
-    queue = []
-    for row in range(n):
-        for col in range(n):
-            queue.append((row, col))
-    
-    return queue
+    def getValue(self, row, col) -> int:
+        if row in self.coordToValueMap and col in self.coordToValueMap[row]:
+            return self.coordToValueMap[row][col]
+        return 0
 
-def get(mat, coord):
-    row = coord[0]
-    col = coord[1]
-    return mat[row][col]
+    def __str__(self) -> str:
+        strings = []
+        for row in range(0, self.length):
+            for col in range(0, self.length):
+                strings.append(self.getValue(row, col).__str__())
+                strings.append(", ")
+            strings.append("\n")
+        return "".join(strings)
 
-def setVal(mat, coord, value):
-    row = coord[0]
-    col = coord[1]
-    mat[row][col] = value
+def projectCoordinate(row, col, length) -> (int, int):
+    return (length - 1 - col, row)
 
-def rotateMat(mat):
-    n = len(mat)
-    queue = getQueue(n)
-    temp = None
-    tempCoord = None
-    finishedCoords = {}
+def rotateMat(matrix: SquareMatrix) -> None:
+    rings = matrix.length // 2
+    for ring in range(0, rings):
+        for relativeSideValue in range(0, matrix.length - ring - 1):
+            topCoordinate = (ring, ring + relativeSideValue)
+            rightCoordinate = (ring + relativeSideValue, matrix.length - ring - 1)
+            bottomCoordinate = (matrix.length - ring - 1, matrix.length - ring - 1 - relativeSideValue)
+            leftCoordinate = (matrix.length - ring - 1 - relativeSideValue, ring)
 
-    while queue:
-        currentCoord = queue.pop() if not tempCoord else tempCoord
-        currentVal = get(mat, currentCoord) if not temp else temp
+            # Save top for now
+            tempValue = matrix.getValue(topCoordinate[0], topCoordinate[1])
 
-        if currentCoord in finishedCoords:
-            temp = None
-            tempCoord = None
-            continue
+            # Override top with left coordinate
+            matrix.setValue(topCoordinate[0], topCoordinate[1], matrix.getValue(leftCoordinate[0], leftCoordinate[1]))
 
-        destCoord = getNextCord(currentCoord, n)
-        temp = get(mat, destCoord)
-        tempCoord = destCoord
-        setVal(mat, destCoord, currentVal)
-        finishedCoords[currentCoord] = True
+            # Override left coordinate with bottom coordinate
+            matrix.setValue(leftCoordinate[0], leftCoordinate[1], matrix.getValue(bottomCoordinate[0], bottomCoordinate[1]))
 
-def runTestCase(mat):
-    rotateMat(mat)
-    printMat(mat)
-    print()
+            # Override bottom coordinate with right coordinate
+            matrix.setValue(bottomCoordinate[0], bottomCoordinate[1], matrix.getValue(rightCoordinate[0], rightCoordinate[1]))
 
-testCase1 = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-]
+            # Override right coordinate with temp
+            matrix.setValue(rightCoordinate[0], rightCoordinate[1], tempValue)
 
-runTestCase(testCase1)
+def testCase0() -> None:
+    testCase = SquareMatrix(3)
+    testCase.setValue(0, 0, 1)
+    testCase.setValue(0, 1, 2)
+    testCase.setValue(0, 2, 3)
+
+    testCase.setValue(1, 0, 4)
+    testCase.setValue(1, 1, 5)
+    testCase.setValue(1, 2, 6)
+
+    testCase.setValue(2, 0, 7)
+    testCase.setValue(2, 1, 8)
+    testCase.setValue(2, 2, 9)
+
+    print(testCase)
+    rotateMat(testCase)
+    print(testCase)
+
+testCase0()
