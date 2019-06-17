58cef7b21bb129a587f82d56a697073c1f6a8e9f
Andrew Johnson <helios2k6@gmail.com>
2019-06-16 19:05:37 -0700

    Fixed bug in rotational logic

--- rotateMatrix.py
+++ rotateMatrix.py
@@ -28,11 +28,12 @@ def projectCoordinate(row, col, length) -> (int, int):
 def rotateMat(matrix: SquareMatrix) -> None:
     rings = matrix.length // 2
     for ring in range(0, rings):
-        for relativeSideValue in range(0, matrix.length - ring - 1):
+        lengthOfRing = matrix.length - ring
+        for relativeSideValue in range(ring, lengthOfRing - 1):
             topCoordinate = (ring, ring + relativeSideValue)
-            rightCoordinate = (ring + relativeSideValue, matrix.length - ring - 1)
-            bottomCoordinate = (matrix.length - ring - 1, matrix.length - ring - 1 - relativeSideValue)
-            leftCoordinate = (matrix.length - ring - 1 - relativeSideValue, ring)
+            rightCoordinate = (ring + relativeSideValue, lengthOfRing - 1)
+            bottomCoordinate = (lengthOfRing - 1, lengthOfRing - 1 - relativeSideValue)
+            leftCoordinate = (lengthOfRing - 1 - relativeSideValue, ring)
 
             # Save top for now
             tempValue = matrix.getValue(topCoordinate[0], topCoordinate[1])
@@ -49,56 +50,60 @@ def rotateMat(matrix: SquareMatrix) -> None:
             # Override right coordinate with temp
             matrix.setValue(rightCoordinate[0], rightCoordinate[1], tempValue)
 
+def testCore(matrix: SquareMatrix) -> None:
+    print(matrix)
+    rotateMat(matrix)
+    print(matrix)
+
 def testCase0() -> None:
-    testCase = SquareMatrix(3)
-    testCase.setValue(0, 0, 1)
-    testCase.setValue(0, 1, 2)
-    testCase.setValue(0, 2, 3)
+    matrix = SquareMatrix(3)
+    matrix.setValue(0, 0, 1)
+    matrix.setValue(0, 1, 2)
+    matrix.setValue(0, 2, 3)
 
-    testCase.setValue(1, 0, 4)
-    testCase.setValue(1, 1, 5)
-    testCase.setValue(1, 2, 6)
+    matrix.setValue(1, 0, 4)
+    matrix.setValue(1, 1, 5)
+    matrix.setValue(1, 2, 6)
 
-    testCase.setValue(2, 0, 7)
-    testCase.setValue(2, 1, 8)
-    testCase.setValue(2, 2, 9)
+    matrix.setValue(2, 0, 7)
+    matrix.setValue(2, 1, 8)
+    matrix.setValue(2, 2, 9)
 
-    print(testCase)
-    rotateMat(testCase)
-    print(testCase)
+    testCore(matrix)
 
 def testCase1() -> None:
-    testCase = SquareMatrix(2)
-    testCase.setValue(0, 0, 1)
-    testCase.setValue(0, 1, 2)
+    matrix = SquareMatrix(2)
+    matrix.setValue(0, 0, 1)
+    matrix.setValue(0, 1, 2)
 
-    testCase.setValue(1, 0, 4)
-    testCase.setValue(1, 1, 5)
+    matrix.setValue(1, 0, 4)
+    matrix.setValue(1, 1, 5)
 
-    print(testCase)
-    rotateMat(testCase)
-    print(testCase)
+    testCore(matrix)
 
 def testCase2() -> None:
-    testCase = SquareMatrix(4)
-    testCase.setValue(0, 0, 1)
-    testCase.setValue(0, 1, 2)
-    testCase.setValue(0, 2, 3)
-    testCase.setValue(0, 3, 4)
+    matrix = SquareMatrix(4)
+    matrix.setValue(0, 0, 1)
+    matrix.setValue(0, 1, 2)
+    matrix.setValue(0, 2, 3)
+    matrix.setValue(0, 3, 4)
 
-    testCase.setValue(1, 0, 5)
-    testCase.setValue(1, 1, 6)
-    testCase.setValue(1, 2, 7)
-    testCase.setValue(1, 3, 8)
+    matrix.setValue(1, 0, 5)
+    matrix.setValue(1, 1, 6)
+    matrix.setValue(1, 2, 7)
+    matrix.setValue(1, 3, 8)
 
-    testCase.setValue(2, 0, 9)
-    testCase.setValue(2, 1, 10)
-    testCase.setValue(2, 2, 11)
-    testCase.setValue(2, 3, 12)
+    matrix.setValue(2, 0, 9)
+    matrix.setValue(2, 1, 10)
+    matrix.setValue(2, 2, 11)
+    matrix.setValue(2, 3, 12)
 
-    print(testCase)
-    rotateMat(testCase)
-    print(testCase)
+    matrix.setValue(3, 0, 13)
+    matrix.setValue(3, 1, 14)
+    matrix.setValue(3, 2, 15)
+    matrix.setValue(3, 3, 16)
+
+    testCore(matrix)
 
 testCase0()
 testCase1()
