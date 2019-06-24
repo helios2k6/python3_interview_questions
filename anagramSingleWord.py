a5e6fb9240da913386cd223dbde663b43e07b97b
Andrew Johnson <abjohnson@fb.com>
2019-06-24 13:15:49 -0700

    Finished adding bucketing to the anagram problem

--- anagramSingleWord.py
+++ anagramSingleWord.py
@@ -1,51 +1,67 @@
-def generateSubstrings(s: str) -> list:
-    substrings = []
+class CharacterBag:
+    def __init__(self, string: str):
+        self.characterBag = {}
+        self.memoizedSizeOfCharacterBag = None
+        for char in string:
+            if char not in self.characterBag:
+                self.characterBag[char] = 0
+            self.characterBag[char] += 1
+
+    def size(self) -> int:
+        if self.memoizedSizeOfCharacterBag:
+            return self.memoizedSizeOfCharacterBag
+
+        self.memoizedSizeOfCharacterBag = 0
+        for numChars in self.characterBag.values():
+            self.memoizedSizeOfCharacterBag += numChars
+
+        return self.memoizedSizeOfCharacterBag
+
+    def isEqual(self, other: "CharacterBag") -> bool:
+        if self.size() != other.size():
+            return False
+        for char, numChars in self.characterBag.items():
+            if char not in other.characterBag or other.characterBag[char] != numChars:
+                return False
+        return True
+
+def generateSubstringMap(s: str) -> dict:
+    substrings = {}
     lengthOfS = len(s)
     for i in range(0, lengthOfS):
         for j in range(i + 1, lengthOfS + 1):
-            substrings.append(s[i:j])
+            substring = s[i:j]
+            if substring not in substrings:
+                substrings[substring] = 0
+            substrings[substring] += 1
     return substrings
 
-def isAnagram(a: str, b: str, anagramBag: dict) -> bool:
-    if len(a) != len(b):
-        return False
+def projectCharacterBagToCountList(substringMap: dict) -> list:
+    characterBagList = []
+    for substring, substringCount in substringMap.items():
+        characterBag = CharacterBag(substring)
+        for i in range(0, substringCount):
+            characterBagList.append(characterBag)
+    return characterBagList
 
-    if a in anagramBag and b in anagramBag[a]:
-        return True
-
-    aStrCharMap = {}
-    bStrCharMap = {}
-    for ac in a:
-        if ac not in aStrCharMap:
-            aStrCharMap[ac] = 0
-        aStrCharMap[ac] += 1
-
-    for bc in b:
-        if bc not in bStrCharMap:
-            bStrCharMap[bc] = 0
-        bStrCharMap[bc] += 1
-
-    for ac, acCount in aStrCharMap.items():
-        if ac not in bStrCharMap or bStrCharMap[ac] != acCount:
-            return False
-
-    if a not in anagramBag:
-        anagramBag[a] = {}
-    if b not in anagramBag:
-        anagramBag[b] = {}
-    
-    anagramBag[a][b] = True
-    anagramBag[b][a] = True
-    return True
+def bucketCharacterBagList(characterBagList: list) -> dict:
+    buckets = {}
+    for characterBag in characterBagList:
+        sizeOfBag = characterBag.size()
+        if sizeOfBag not in buckets:
+            buckets[sizeOfBag] = []
+        buckets[sizeOfBag].append(characterBag)
+    return buckets
 
 def numberOfAnagrams(s: str) -> int:
-    anagramBagMap = {}
-    substrings = generateSubstrings(s)
+    bucketsOfCharacterBagLists = bucketCharacterBagList(projectCharacterBagToCountList(generateSubstringMap(s)))
     anagramCount = 0
-    for i in range(0, len(substrings) - 1):
-        for j in range(i + 1, len(substrings)):
-            if isAnagram(substrings[i], substrings[j], anagramBagMap):
-                anagramCount += 1
+    for listOfBags in bucketsOfCharacterBagLists.values():
+        numberOfCharacterBags = len(listOfBags)
+        for i in range(0, numberOfCharacterBags - 1):
+            for j in range(i + 1, numberOfCharacterBags):
+                if listOfBags[i].isEqual(listOfBags[j]):
+                    anagramCount += 1
     return anagramCount
 
 def testCore(s: str) -> None:
