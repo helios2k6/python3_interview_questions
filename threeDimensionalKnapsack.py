37d1e113d4cc8a5636006fc5e85daf8e4009a618
Andrew Johnson <abjohnson@fb.com>
2019-07-11 16:55:28 -0700

    Successfully added category limits

--- threeDimensionalKnapsack.py
+++ threeDimensionalKnapsack.py
@@ -4,12 +4,16 @@ class Food:
         self.protein = protein
         self.carbs = carbs
         self.fat = fat
+        self.category = None
 
     def calories(self) -> int:
         return (self.fat * 9) + (self.protein * 4) + (self.carbs * 4)
 
     def __str__(self) -> str:
         return self.name
+
+    def setCategory(self, cat: str) -> None:
+        self.category = cat
 
 class SparseCache:
     def __init__(self):
@@ -31,29 +35,40 @@ class SparseCache:
             self.cache[itemNumber][protein][carbs][fat] = {}
         self.cache[itemNumber][protein][carbs][fat] = (valueProtein, valueCarbs, valueFat)
 
+    def __str__(self):
+        return self.cache.__str__()
+
 def tryGetFromCache(cache: dict, itemNumber: int, protein: int, carbs: int, fat: int):
     if itemNumber not in cache or protein not in cache[itemNumber] or carbs not in cache[itemNumber][protein] or fat not in cache[itemNumber][protein][carbs]:
         return (0, 0, 0)
     return cache[itemNumber][protein][carbs][fat]
 
-def solveKnapsack3D(foods: list, protein: int, carbs: int, fat: int) -> list:
+def solveKnapsack3D(foods: list, protein: int, carbs: int, fat: int, categoryLimits: dict) -> list:
     # Initialize the cache first
     cache = SparseCache()
+    foodCategoryTallies = {}
     for ithFood in range(0, len(foods)):
         food = foods[ithFood]
+        if food.category and food.category in categoryLimits:
+            if food.category not in foodCategoryTallies:
+                foodCategoryTallies[food.category] = 0
+            foodCategoryTallies[food.category] += 1
         for p in range(0, protein + 1):
             for c in range(0, carbs + 1):
                 for f in range(0, fat + 1):
                     previousValue = cache.tryGet(ithFood - 1, p, c, f)
-                    if food.protein > p or food.carbs > c or food.fat > f:
-                        # Food exceeds our knapsack limit
+                    # We will special case protein. We're OK with exceeding protein levels. This isn't true 3D knapsack since we have
+                    # special rules in place for specific macros. We also need to respect "food category limits" so that we don't just add
+                    # a bunch of protein shakes into the diet. This also complicates our diet plan
+                    if food.carbs > c or food.fat > f or (food.category and food.category in categoryLimits and food.category in foodCategoryTallies and foodCategoryTallies[food.category] > categoryLimits[food.category]):
+                        # Food exceeds our knapsack limit or has limits placed on it
                         cache.insert(ithFood, p, c, f, previousValue[0], previousValue[1], previousValue[2])
                     else:
                         # Food does not exceed our limit. Decide which is more valuable, keeping the previous food item
                         # or adding our current food item
                         valueWithoutPreviousFood = cache.tryGet(ithFood - 1, p - food.protein, c - food.carbs, f - food.fat)
                         valueWithoutPrevousFoodButWithCurrentFood = (valueWithoutPreviousFood[0] + food.protein, valueWithoutPreviousFood[1] + food.carbs, valueWithoutPreviousFood[2] + food.fat)
-                        if previousValue[0] < valueWithoutPrevousFoodButWithCurrentFood[0] and previousValue[1] < valueWithoutPrevousFoodButWithCurrentFood[1] and previousValue[2] < valueWithoutPrevousFoodButWithCurrentFood[2]:
+                        if previousValue[0] < valueWithoutPrevousFoodButWithCurrentFood[0] or previousValue[1] < valueWithoutPrevousFoodButWithCurrentFood[1] or previousValue[2] < valueWithoutPrevousFoodButWithCurrentFood[2]:
                             cache.insert(ithFood, p, c, f, valueWithoutPrevousFoodButWithCurrentFood[0], valueWithoutPrevousFoodButWithCurrentFood[1], valueWithoutPrevousFoodButWithCurrentFood[2])
                         else:
                             cache.insert(ithFood, p, c, f, previousValue[0], previousValue[1], previousValue[2])
@@ -65,6 +80,7 @@ def solveKnapsack3D(foods: list, protein: int, carbs: int, fat: int) -> list:
     for itemNumber in reversed(range(0, len(foods))):
         currentValue = cache.tryGet(itemNumber, currentProteinLimit, currentCarbsLimit, currentFatLimit)
         previousValue = cache.tryGet(itemNumber - 1, currentProteinLimit, currentCarbsLimit, currentFatLimit)
+
         if currentValue[0] != previousValue[0] or currentValue[1] != previousValue[1] or currentValue[2] != previousValue[2]:
             currentFood = foods[itemNumber]
             listOfItems.append(currentFood)
@@ -76,6 +92,34 @@ def solveKnapsack3D(foods: list, protein: int, carbs: int, fat: int) -> list:
 
     return listOfItems
 
+def dietCore(foods: list, proteinLimit: int, carbLimit: int, fatLimit: int, categoryLimits: dict):
+    print(f"Testing with: ({proteinLimit}, {carbLimit}, {fatLimit})")
+    optimalFoods = solveKnapsack3D(foods, proteinLimit, carbLimit, fatLimit, categoryLimits)
+    runningProtein = 0
+    runningCarbs = 0
+    runningFat = 0
+    for food in optimalFoods:
+        runningProtein += food.protein
+        runningCarbs += food.carbs
+        runningFat += food.fat
+        print(f"{food} | Total Protein: {runningProtein} | Total Carbs: {runningCarbs} | Total Fat: {runningFat}")
+
+
+def diet0():
+    foods = []
+
+    for _ in range(0, 3):
+        f = Food("A", 1, 1, 1)
+        f.setCategory("A")
+        foods.append(f)
+
+    for _ in range(0, 1):
+        f = Food("B", 1, 1, 1)
+        f.setCategory("B")
+        foods.append(f)
+
+    dietCore(foods, 3, 3, 3, {"A": 2})
+
 def diet1():
     foods = []
     # Scrambled Eggs
@@ -84,7 +128,9 @@ def diet1():
 
     # Muscle Milk
     for _ in range(0, 2):
-        foods.append(Food("Muscle Milk (14 oz)", 25, 9, 5))
+        shake = Food("Muscle Milk (14 oz)", 25, 9, 5)
+        shake.setCategory("Shake")
+        foods.append(shake)
 
     # Bacon
     for _ in range(0, 4):
@@ -96,7 +142,9 @@ def diet1():
 
     # Premier Protein Shake
     for _ in range(0, 2):
-        foods.append(Food("Premier Protein Shake (325 ml)", 30, 5, 3))
+        shake = Food("Premier Protein Shake (325 ml)", 30, 5, 3)
+        shake.setCategory("Shake")
+        foods.append(shake)
 
     # Hanger Steak
     for _ in range(0, 2):
@@ -110,14 +158,6 @@ def diet1():
     for _ in range(0, 4):
         foods.append(Food("Lactaid Milk (1 cup)", 8, 13, 5))
 
-    optimalFoods = solveKnapsack3D(foods, 160, 166, 101)
-    runningProtein = 0
-    runningCarbs = 0
-    runningFat = 0
-    for food in optimalFoods:
-        runningProtein += food.protein
-        runningCarbs += food.carbs
-        runningFat += food.fat
-        print(f"{food} | Total Protein: {runningProtein} | Total Carbs: {runningCarbs} | Total Fat: {runningFat}")
+    dietCore(foods, 160, 166, 101, {"Shake": 2})
 
-diet1()
+diet0()
