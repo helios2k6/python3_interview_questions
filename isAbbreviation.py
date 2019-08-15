d5aaeea2437dfb8066a175e1fde4558e3103b9a7
Andrew Johnson <abjohnson@fb.com>
2019-08-15 13:08:58 -0700

    Added DP solution

--- isAbbreviation.py
+++ isAbbreviation.py
@@ -4,41 +4,16 @@ def isAbbreviation(a: str, b: str) -> bool:
     if not a or not b or len(a) < len(b):
         return False
 
-    cache = [[False for _ in range(0, len(b))] for _ in range(0, len(a))]
-    lowercaseMatchCache = [[False for _ in range(0, len(b))] for _ in range(0, len(a))]
-    lowerCaseLetters = [False for _ in range(0, len(a))]
-    for bIndex in range(0, len(b)):
-        for aIndex in range(bIndex, len(a)):
-            if str.islower(a[aIndex]):
-                lowerCaseLetters[aIndex] = True
-            if a[aIndex] == b[bIndex]:
-                cache[aIndex][bIndex] = True
-            elif str.upper(a[aIndex]) == b[bIndex]:
-                lowercaseMatchCache[aIndex][bIndex] = True
-
-    executionStack = [(0, 0)]
-    checkedPoints = [[False for _ in range(0, len(b))] for _ in range(0, len(a))]
-    while executionStack:
-        aIndex, bIndex = executionStack.pop()
-        if aIndex > len(a) or bIndex > len(b) or checkedPoints[aIndex][bIndex]:
-            continue
-
-        checkedPoints[aIndex][bIndex] = True
-        if aIndex == len(a) - 1 and bIndex == len(b) - 1:
-            return cache[aIndex][bIndex] or lowercaseMatchCache[aIndex][bIndex] or lowerCaseLetters[aIndex]
-        if cache[aIndex][bIndex]:
-            # Must advance both indices
-            executionStack.append((aIndex + 1, bIndex + 1))
-        elif lowercaseMatchCache[aIndex][bIndex]:
-            # Could advance aIndex and bIndex
-            executionStack.append((aIndex + 1, bIndex + 1))
-            # Could advance aIndex only
-            executionStack.append((aIndex + 1, bIndex))
-        elif lowerCaseLetters[aIndex]:
-            # We could advance the aIndex
-            executionStack.append((aIndex + 1, bIndex))
-
-    return False
+    isValid = [[False for _ in range(0, len(b) + 1)] for _ in range(0, len(a) + 1)]
+    isValid[0][0] = True
+    for i in range(0, len(a)):
+        for j in range(0, len(b) + 1):
+            if isValid[i][j]:
+                if j < len(b) and str.upper(a[i]) == b[j]:
+                    isValid[i + 1][j + 1] = True
+                if not str.isupper(a[i]):
+                    isValid[i + 1][j] = True
+    return isValid[len(a)][len(b)]
 
 def testCore(a: str, b: str) -> None:
     print(isAbbreviation(a, b))
@@ -83,10 +58,10 @@ def test7() -> None:
         "XBOBBOBOXOXBOXOOBOXOXOBBXOXBXOXXBBOBXOXXXOBBXBOOOXXOXBBBXOXOOOXXOBBOXXOBBXXXOXXXOXXOOXOXBOBBBXBBXOOXOBXXOOOOBXBOXXBXBXXXBXOBBOBBXXOXXBOBBXOXXBBOOOBBBOXBBBOXXBXXOBOBXOOOXXXXXXOBXXBOXXOOOOBOOOXBBOOBXOXXOBBBBOOXXOOXXXOBBXXOXBBOXOXBBBOXOBXXXBXXOBBXBOOBBBOXBBBOXBXBOBBXXXOXBOOXOOXBOXXOOOOBBBBOOBBOBOOBOBXBBOXBOBOXOXBXOBBOBBOXBOOXXBBBBBXOBXOBXXXBBBXOOBOOOXOOBBBXXOXXOXOBOXOBXXOXOOXXXOXXOBOOXBBXBOXBXXOXOXBOBXXOOXOOOXXBOOBBXXXBBOXBBXBOBBOOBOOXOXXBXOBOOOXBOXOOXOOOBBOBBOOOBBBBBOOBOXBBBOBOBXOXBXOBXBXBXBBBXOOO"
     )
 
-#test1()
+test1()
 test2()
-#test3()
-#test4()
-#test5()
-#test6()
-#test7()
+test3()
+test4()
+test5()
+test6()
+test7()
