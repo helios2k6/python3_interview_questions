fe46932858a197a4395f7dd34531098ecb6b99e8
Andrew Johnson <helios2k6@gmail.com>
2019-08-15 07:50:58 -0700

    Updating check against cache, doesn't work yet

--- isAbbreviation.py
+++ isAbbreviation.py
@@ -5,39 +5,40 @@ def isAbbreviation(a: str, b: str) -> bool:
         return False
 
     cache = [[False for _ in range(0, len(b))] for _ in range(0, len(a))]
+    lowercaseMatchCache = [[False for _ in range(0, len(b))] for _ in range(0, len(a))]
     lowerCaseLetters = [False for _ in range(0, len(a))]
     for bIndex in range(0, len(b)):
         for aIndex in range(bIndex, len(a)):
             if str.islower(a[aIndex]):
                 lowerCaseLetters[aIndex] = True
-            if str.upper(a[aIndex]) == b[bIndex]:
+            if a[aIndex] == b[bIndex]:
                 cache[aIndex][bIndex] = True
+            elif str.upper(a[aIndex]) == b[bIndex]:
+                lowercaseMatchCache[aIndex][bIndex] = True
 
-    # For every character in "b", find a match in "a"
-    for bIndex in range(0, len(b)):
-        didFindMatch = False
-        for aIndex in range(bIndex, len(a)):
-            if cache[aIndex][bIndex]:
-                didFindMatch = True
-                break
-        if not didFindMatch:
-            return False
+    executionStack = [(0, 0)]
+    checkedPoints = [[False for _ in range(0, len(b))] for _ in range(0, len(a))]
+    while executionStack:
+        aIndex, bIndex = executionStack.pop()
+        if aIndex > len(a) or bIndex > len(b) or checkedPoints[aIndex][bIndex]:
+            continue
 
-    # check to make sure that any non-lowercase letters are also consumed by some 
-    # character in "b"
-    takenBChars = [False for _ in range(0, len(b))]
-    for lowerIndex in range(0, len(a)):
-        if not lowerCaseLetters[lowerIndex]:
-            foundBMatch = False
-            for bIndex in range(0, len(b)):
-                if cache[lowerIndex][bIndex] and not takenBChars[bIndex]:
-                    foundBMatch = True
-                    takenBChars[bIndex] = True
-                    break
-            if not foundBMatch:
-                return False
+        checkedPoints[aIndex][bIndex] = True
+        if aIndex == len(a) - 1 and bIndex == len(b) - 1:
+            return cache[aIndex][bIndex] or lowercaseMatchCache[aIndex][bIndex] or lowerCaseLetters[aIndex]
+        if cache[aIndex][bIndex]:
+            # Must advance both indices
+            executionStack.append((aIndex + 1, bIndex + 1))
+        elif lowercaseMatchCache[aIndex][bIndex]:
+            # Could advance aIndex and bIndex
+            executionStack.append((aIndex + 1, bIndex + 1))
+            # Could advance aIndex only
+            executionStack.append((aIndex + 1, bIndex))
+        elif lowerCaseLetters[aIndex]:
+            # We could advance the aIndex
+            executionStack.append((aIndex + 1, bIndex))
 
-    return True
+    return False
 
 def testCore(a: str, b: str) -> None:
     print(isAbbreviation(a, b))
@@ -83,9 +84,9 @@ def test7() -> None:
     )
 
 #test1()
-#test2()
+test2()
 #test3()
 #test4()
 #test5()
 #test6()
-test7()
+#test7()
